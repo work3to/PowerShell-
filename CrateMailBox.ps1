898eedfa847413826b487a3609f670896b36e0e6
work3to <57717139+work3to@users.noreply.github.com>
2019-11-13 20:47:19 +0200

    scripts

---
+++ CrateMailBox.ps1
@@ -0,0 +1,3 @@
+﻿D:\v1.0\powershell.exe -NoExit -ImportSystemModules
+$user = Read-Host –Prompt 'Enter the user name you want to crate: '
+New-Mailbox -name $user -Database UsersMailboxDatabase 
---
+++ CreateUserAndMailbox.ps1
@@ -0,0 +1,21 @@
+﻿#create usar and mailbox
+################
+#Created by:
+#Date: 13.11.2019
+#Version 1.0.0
+#
+################
+
+import-Module ActiveDirectory
+
+# Variables :::::::::::::::::::::::::::::::::::::::::::::::::
+$CopyFrom = "035304323"
+$usertocreate = Read-Host –Prompt 'Enter the user name you want to crate: '
+$Des = Read-Host –Prompt 'Enter the Description you want to add: '
+$pass = "123456"
+
+Get-ADUser -Identity $CopyFrom -Properties * 
+$usertocreate = Get-ADUser -Identity $CopyFrom -Properties *
+New-ADUser -Name $usertocreate -CannotChangePassword  $True -ChangePasswordAtLogon $false -Description $Des -Enabled $True -Instance $CopyFrom -PasswordNeverExpires $True
+ 
+ 
